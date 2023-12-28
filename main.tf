eca04e862f958d30930721cb811207abab668d87
Sourav Kumar <souravk326@gmail.com>
2023-12-28 09:57:48 +0000

    Simple S3 Bucket #5 added s3 bucket

--- main.tf
+++ main.tf
@@ -4,7 +4,15 @@ terraform {
         source = "hashicorp/random"
         version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.31.0"
+    }
   }
+}
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
@@ -12,10 +20,17 @@ provider "random" {
 }
 
 resource "random_string" "bucket_name" {
+  lower = true
+  upper = false
   length = 16
   special = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.id
 }
 
 output "random_bucket_name_id" {
   value = random_string.bucket_name.id
 }
+
