25a7b5b3111e17541e0189fe54d1af61a642a03c
Sourav Kumar <souravk326@gmail.com>
2024-01-03 19:35:28 +0530

    upload the state file to terraform cloud #7 (#8)

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 terraform {
+
+cloud {
+    organization = "terratown"
+
+    workspaces {
+      name = "terratown-1"
+    }
+  }
+
   required_providers {
     random = {
         source = "hashicorp/random"
