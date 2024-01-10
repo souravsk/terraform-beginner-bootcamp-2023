4d41ba1090925aa50fa530392429fbe42e78ef48
Sourav Kumar <souravk326@gmail.com>
2024-01-10 12:25:54 +0000

    commanted some code - test-2

--- output.tf
+++ output.tf
@@ -8,10 +8,10 @@ output "s3_website_endpoint" {
   value = module.terrahouse_aws.website_endpoint
 }
 
-output "cloudfront_url" {
-  description = "The CloudFront Distribution Domain Name"
-  value = module.terrahouse_aws.cloudfront_url
-}
+# output "cloudfront_url" {
+#   description = "The CloudFront Distribution Domain Name"
+#   value = module.terrahouse_aws.cloudfront_url
+# }
 
 locals {
   root_path = path.root
