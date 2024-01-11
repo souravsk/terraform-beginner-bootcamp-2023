e919a6a4318dad95d95b80538f8594672238c387
Sourav Kumar <souravk326@gmail.com>
2024-01-11 20:33:20 +0530

    34 terratowns provider with golang (#36)
    * Terratowns Provider with golang #34
    
    * made some change

--- output.tf
+++ output.tf
@@ -1,22 +1,22 @@
-output "bucket_name" {
-  description = "Bucket name for our static website hosting"
-  value = module.terrahouse_aws.bucket_name
-}
-
-output "s3_website_endpoint" {
-  description = "S3 Static Website hosting endpoint"
-  value = module.terrahouse_aws.website_endpoint
-}
-
-# output "cloudfront_url" {
-#   description = "The CloudFront Distribution Domain Name"
-#   value = module.terrahouse_aws.cloudfront_url
+# output "bucket_name" {
+#   description = "Bucket name for our static website hosting"
+#   value = module.terrahouse_aws.bucket_name
 # }
 
-locals {
-  root_path = path.root
-}
+# output "s3_website_endpoint" {
+#   description = "S3 Static Website hosting endpoint"
+#   value = module.terrahouse_aws.website_endpoint
+# }
 
-output "root_path" {
-  value = local.root_path
-}
+# # output "cloudfront_url" {
+# #   description = "The CloudFront Distribution Domain Name"
+# #   value = module.terrahouse_aws.cloudfront_url
+# # }
+
+# locals {
+#   root_path = path.root
+# }
+
+# output "root_path" {
+#   value = local.root_path
+# }
